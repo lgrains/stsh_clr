4e257a577e6aeb67f2a5c5ed7ce60b151d54b9f5
Louise Rains <lrains@berklee.edu>
2013-10-03 11:22:33 -0400

    adds Fabrication, Faker and user model spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,61 +1,52 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe User do
-  def new_user(attributes = {})
-    attributes[:email] ||= 'foo@example.com'
-    attributes[:password] ||= 'abc123'
-    attributes[:password_confirmation] ||= attributes[:password]
-    User.new(attributes)
-  end
+  subject { Fabricate(:user) }
+  it { should be_valid }
 
-  before(:each) do
-    User.delete_all
-  end
+  #associations
 
-  it "should be valid" do
-    new_user.should be_valid
-  end
+  #validations
+  it { should validate_presence_of :email }
 
   it "should require email" do
-    new_user(:email => '').should have(1).error_on(:email)
+    Fabricate.build(:user, email: '').should_not be_valid
   end
 
   it "should require password" do
-    new_user(:password => '').should have(1).error_on(:password)
+    Fabricate.build(:user, :password => '').should_not be_valid
   end
 
   it "should require well formed email" do
-    new_user(:email => 'foo@bar@example.com').should have(1).error_on(:email)
+    Fabricate.build(:user, :email => 'foo@bar@example.com').should have(1).error_on(:email)
   end
 
   it "should validate uniqueness of email" do
-    new_user(:email => 'bar@example.com').save!
-    new_user(:email => 'bar@example.com').should have(1).error_on(:email)
+    Fabricate(:user, :email => 'bar_s@example.com')
+    Fabricate.build(:user, :email => 'bar_s@example.com').should_not be_valid
   end
 
   it "should validate password is longer than 3 characters" do
-    new_user(:password => 'bad').should have(1).error_on(:password)
+    Fabricate.build(:user, password: 'bad', password_confirmation: 'bad').should_not be_valid
   end
 
-  # it "should require matching password confirmation" do
-  #   Fabricate(:user, :password_confirmation => 'nonmatching').should_not be_valid
-  # end
+  it "should require matching password confirmation" do
+    Fabricate.build(:user, password_confirmation: 'nonmatching').should_not be_valid
+  end
 
   it "should generate password hash and salt on create" do
-    user = new_user
-    user.save!
+    user = Fabricate(:user)
     user.password_hash.should_not be_nil
     user.password_salt.should_not be_nil
   end
 
   it "should authenticate by email" do
-    user = new_user(:email => 'foo@bar.com', :password => 'secret')
-    user.save!
+    user = Fabricate(:user, email: 'foo@bar.com', password: 'secret', password_confirmation: 'secret')
     User.authenticate('foo@bar.com', 'secret').should == user
   end
 
   it "should not authenticate bad password" do
-    new_user(:password => 'secret').save!
+    Fabricate(:user, password: 'secret', password_confirmation: 'secret')
     User.authenticate('foobar', 'badpassword').should be_nil
   end
 end
